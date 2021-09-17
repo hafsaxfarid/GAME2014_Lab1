6ae6f10fe16f8104c0bd46cbe7405bd1f905ce0c
hafsaxfarid <59932046+hafsaxfarid@users.noreply.github.com>
2021-09-17 00:39:19 -0400

    StartButton
    Added a start button that displays a debug log stating game is starting

---
+++ GAME2014_Lab1_HafsaFarid/Assets/Scripts/StartManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{
+    public void GameStart()
+    {
+        Debug.Log("Starting Game...");
+    }
+}
